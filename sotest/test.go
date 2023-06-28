f27a3d50096cfed89d9f69697eba9218ddcc063e
Bill Mill <bill@billmill.org>
2023-06-28 10:52:24 -0400

    give up for now on the search path and stick libwhisper in this dir

--- sotest/test.go
+++ sotest/test.go
@@ -2,10 +2,15 @@ package main
 
 // #include <stdio.h>
 // #include <whisper/whisper.h>
-// #cgo LDFLAGS: -L./whisper -l whisper
-// #cgo LDFLAGS: -Wl,-rpath,@executable_path/whisper/
+// #cgo CFLAGS: -I ${SRCDIR}/whisper
+// #cgo LDFLAGS: -L ${SRCDIR}/whisper -Wl,-rpath ${SRCDIR}/whisper -lwhisper
 import "C"
 import "fmt"
+
+// TODO: for some reason, I absolutely cannot get the runtime to search the
+// whisper directory using the `rpath` argument. I've tried many little options
+// and I'm certain it's not a big change but it's killing me. I'm giving up for
+// now and sticking `libwhisper.so` in the same dir as the binary
 
 func main() {
 	// The function call we're going to try and reproduce is:
